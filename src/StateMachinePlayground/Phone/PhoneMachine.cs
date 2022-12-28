e4f53aa840dae0b7bf63766ca9da47584bcdea8a
Ravi Khubchandani <Ravi.Khubchandani@axpo.com>
2022-12-28 18:23:45 +0100

    ignores reentering same states and adds operations to perform on machine

--- src/StateMachinePlayground/Phone/PhoneMachine.cs
+++ src/StateMachinePlayground/Phone/PhoneMachine.cs
@@ -17,20 +17,35 @@
                     .Permit(PhoneActions.TurnOff, PhoneStates.Off)
                     .Permit(PhoneActions.PickUp, PhoneStates.Busy)
                     .Permit(PhoneActions.SendCall, PhoneStates.Busy)
-                    .Permit(PhoneActions.GetCall, PhoneStates.Ringing);
+                    .Permit(PhoneActions.GetCall, PhoneStates.Ringing)
+                    .Ignore(PhoneActions.HangUp)
+                    .Ignore(PhoneActions.TurnOn);
+
 
                 machine.Configure(PhoneStates.Ringing)
                     .Permit(PhoneActions.PickUp, PhoneStates.Busy)
-                    .Permit(PhoneActions.HangUp, PhoneStates.StandBy);
+                    .Permit(PhoneActions.HangUp, PhoneStates.StandBy)
+                    .Ignore(PhoneActions.GetCall);
 
                 machine.Configure(PhoneStates.Busy)
-                    .Permit(PhoneActions.HangUp, PhoneStates.StandBy);
+                    .Permit(PhoneActions.HangUp, PhoneStates.StandBy)
+                    .Ignore(PhoneActions.PickUp)
+                    .Ignore(PhoneActions.SendCall);
 
                 machine.Configure(PhoneStates.Off)
-                    .Permit(PhoneActions.TurnOn, PhoneStates.StandBy);
+                    .Permit(PhoneActions.TurnOn, PhoneStates.StandBy)
+                    .Ignore(PhoneActions.TurnOff);
 
                 return machine;
             });
         }
+
+        public PhoneStates Is() => _phone.Value.State;
+
+        public bool Can(PhoneActions action) => _phone.Value.CanFire(action);
+
+        public IEnumerable<PhoneActions> WhatCan() => _phone.Value.PermittedTriggers;
+
+        public void Do(PhoneActions action) => _phone.Value.Fire(action);
     }
 }
